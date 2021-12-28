f1009125e1299a2ed078c6ed00ba728cebdbd932
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2021-12-28 11:16:04 +0300

    aaa

---
+++ src/main/java/ibmtal/com/facebook/api/SharingController.java
@@ -0,0 +1,26 @@
+package ibmtal.com.facebook.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ibmtal.com.facebook.business.services.SharingService;
+import ibmtal.com.facebook.core.result.Result;
+import ibmtal.com.facebook.entity.dto.SharingDto;
+@RestController
+@RequestMapping("/api/sharings")
+public class SharingController {
+	private SharingService sharingService;
+	@Autowired
+	public SharingController(SharingService sharingService) {
+		super();
+		this.sharingService = sharingService;
+	}
+	@PostMapping("/newSharing")
+	public Result newSharing(@RequestBody SharingDto sharingDto) {
+		
+		return sharingService.addSharing(sharingDto);
+	}
+}
